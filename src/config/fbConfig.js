2006d5e8e7c998cc5dd161210782929e9fea7f8f
EGO VICTOR <egovictorc@outlook.com>
2019-08-27 19:20:50 +0100

    resolve env

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: process.env_REACT_APP_API_KEY,
   authDomain: process.env_REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env_REACT_APP_DATABASE_URL,
-  projectId: process.env_REACT_APP_PROJECT_ID,
+  projectId: "testfire-1c2ca",
   storageBucket: process.env_REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env_REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env_REACT_APP_APP_ID,
